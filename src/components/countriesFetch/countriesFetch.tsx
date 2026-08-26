25e8aa019cef5da839cc854b28cf7e5b961a92f7
Iria Luz <irialuzs@gmail.con>
2026-08-26 16:05:15 +0100

    fetch country details

--- src/components/countriesFetch/countriesFetch.tsx
+++ src/components/countriesFetch/countriesFetch.tsx
@@ -1,4 +1,5 @@
 import { useQuery, queryOptions } from "@tanstack/react-query";
+import { useState } from "react";
 
 interface CountriesProps {
     alpha2Code: string
@@ -6,6 +7,11 @@ interface CountriesProps {
     name: string,
 }
 
+interface CountryDetailProps {
+    name: string,
+    alpha2Code: string
+    population: number
+}
 
 const fetchCountries = async ({ signal }: { signal: AbortSignal }) => {
     const res = await fetch(
@@ -21,9 +27,25 @@ const countriesOptions = queryOptions({
     queryFn: fetchCountries,
 })
 
+const fetchCountryByCode = async ({ queryKey, signal }: { queryKey: [string, string], signal: AbortSignal }) => {
+    const res = await fetch(`https://countries.dev/alpha/${queryKey[1]}`, { signal })
+    if (!res.ok) throw new Error(`Server returned ${res.status}`)
+    const json = await res.json() as CountryDetailProps
+    return json
+}
+
 const CountryList = () => {
+    const [countryCode, setCountryCode] = useState("")
+
+    const countryDetails = queryOptions({
+        queryKey: ['country', countryCode],
+        queryFn: fetchCountryByCode,
+        enabled: !!countryCode
+    })
+
 
     const { isPending, isError, data, error } = useQuery(countriesOptions)
+    const { data: dataCountry, isError: dataCountryIsError, isPending: dataCountryIsPending, error: dataCountryError } = useQuery(countryDetails)
 
 
     if (isPending) {
@@ -34,16 +56,23 @@ const CountryList = () => {
         return <span>Error: {error.message}</span>
     }
 
-    console.log(data)
+
 
     return (
-        <div className="flex flex-col items-start text-start gap-2  border-[#2e303a] border max-w-xl p-4">
-
-            <ul>
-                {data.map((country) => (
-                    <li key={country.alpha2Code}>{country.name}{country.flag}</li>
-                ))}
-            </ul>
+        <div className="text-start">
+            <h1>TanSrack practice</h1>
+            <div className="flex flex-row items-start text-start gap-2  border-[#2e303a] border max-w-xl p-4">
+                <ul>
+                    {data.map((country) => (
+                        <li onClick={() => { setCountryCode(country.alpha2Code) }} key={country.alpha2Code}>{country.name}{country.flag}</li>
+                    ))}
+                </ul>
+                {countryCode && dataCountryIsPending && <span>Loading ...</span>}
+                {countryCode && dataCountryIsError && <span>Error: {dataCountryError.message}</span>}
+                {dataCountry && <ul>
+                    <p>{dataCountry.name}, tiene una poblacion de: {dataCountry.population} </p>
+                </ul>}
+            </div>
         </div>
     )
 }

8e6b97baf2b73ea7e6b9fb8659ef5fbe3a106f37
Iria Luz <irialuzs@gmail.con>
2026-09-04 16:17:43 +0100

    map the status

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import "./App.css";
 import Column from "./components/column/column";
 import { mockJobs } from "./mocks/jobs";
+import { STATUSES } from "./types/job";
 function App() {
 
   const filteredJobsByStatus = (jobStatus: string) => mockJobs.filter(j => j.status === jobStatus)
   return (
     <div className="flex flex-row items-start gap-2  border-[#2e303a] border rounded-2xl  p-4">
-      <Column status="saved" jobs={filteredJobsByStatus('saved')} />
-      <Column status="applied" jobs={filteredJobsByStatus('applied')} />
-      <Column status="interviewing" jobs={filteredJobsByStatus('interviewing')} />
-      <Column status="offered" jobs={filteredJobsByStatus('offered')} />
-      <Column status="rejected" jobs={filteredJobsByStatus('rejected')} />
+      {STATUSES.map(status => {
+        return (
+          <Column status={status} jobs={filteredJobsByStatus(status)} />
+        )
+      })}
     </div>
   );
 }

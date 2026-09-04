4972bddd854f09cbc9438ccc561d9379029e3e86
Iria Luz <irialuzs@gmail.con>
2026-09-04 16:56:16 +0100

    styled the column and the card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -4,7 +4,7 @@ import { JobApplication } from '../../types/job'
 export const Card = ({ job }: { job: JobApplication }) => {
     const { company, role, salary, link } = job
     return (
-        <div className="flex flex-col items-start gap-2  border-[#2e303a] border rounded-2xl  p-4">
+        <div className="flex flex-col items-start gap-2  border-[#2e303a] border rounded-2xl bg-[var(--code-bg)] p-4">
             <div>{company}</div>
             <div>{role}</div>
             <div>{salary}</div>
--- src/components/column/column.tsx
+++ src/components/column/column.tsx
@@ -4,10 +4,8 @@ import { Card } from '../card/card'
 
 const Column = ({ status, jobs }: { status: string, jobs: JobApplication[] }) => {
 
-
-
     return (
-        <div className="flex flex-col items-start gap-2">
+        <div className="flex flex-col items-start border rounded-2xl bg-gray-800 gap-2 p-4">
             <h2>{status}</h2>
             {jobs.map(job => {
                 return (

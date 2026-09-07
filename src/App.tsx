b9e942c347ae6281fd0494a7ab34494335c89813
Iria Luz <irialuzs@gmail.con>
2026-09-07 05:51:39 +0100

    refactor board to be a dumb component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import "./App.css";
 import { Board } from "./components/board/board";
-import Column from "./components/column/column";
 import { mockJobs } from "./mocks/jobs";
 import { STATUSES } from "./types/job";
 function App() {
 
-  const filteredJobsByStatus = (jobStatus: string) => mockJobs.filter(j => j.status === jobStatus)
   return (
     <>
-      <Board >
-        {STATUSES.map((status, i) => {
-          return (
-            <Column key={i} status={status} jobs={filteredJobsByStatus(status)} />
-          )
-        })}
-      </Board>
+      <Board statuses={STATUSES} jobs={mockJobs} />
     </>
   );
 }
--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Column from '../column/column'
+import { JobApplication } from '../../types/job'
 
-export const Board = ({ children }: { children: React.ReactNode }) => {
+export const Board = ({ statuses, jobs }: { statuses: readonly string[], jobs: JobApplication[] }) => {
+    const filteredJobsByStatus = (jobStatus: string) => jobs.filter(j => j.status === jobStatus)
+
     return (
-        <div className='flex flex-row gap-4 border-[#2e303a] border rounded-2xl p-4 h-screen overflow-x-auto'>{children}</div>
+        <div className='flex flex-row gap-4 border-[#2e303a] border rounded-2xl p-4 h-screen overflow-x-auto'>
+            {statuses.map((status, i) => {
+                return (
+                    <Column key={i} status={status} jobs={filteredJobsByStatus(status)} />
+                )
+            })}
+        </div>
     )
 }
